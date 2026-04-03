c3b43db0c26799a96d3b81d30756ed8184babeda
BangarYB17 <bangaryogeshwar3@gmail.com>
2026-04-03 09:42:25 +0530

    COLLECTIONS

---
+++ collection/Hashtable_ex.java
@@ -0,0 +1,36 @@
+import java.util.Hashtable;
+
+public class Hashtable_ex {
+    public static void main(String[] args) {
+        Hashtable<Integer , String> table = new Hashtable<>();
+
+        // put()- add elements
+        table.put(1,"java");
+        table.put(2,"python");
+        table.put(3,"c");
+        table.put(4,"sql");
+
+        System.out.println(table);
+
+        //get()
+        System.out.println("value for key 2: "+table.get(2));
+
+        //remove()
+        table.remove(4);
+
+        //containskey()
+        System.out.println("contains key 1" + table.containsKey(2));
+
+        //containsvalue()
+        System.out.println("Contains value java: "+table.containsValue("java"));
+
+        //size()
+        System.out.println("table size:"+table.size());
+
+        //iterator()
+        for(Integer  values :table.keySet()){
+            System.out.println(values+"->"+table.get(values));
+        }
+
+    }
+}
---
+++ collection/LinkedList_ex.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class LinkedList_ex {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("c++");
+        list.add("c#");
+        list.add("python");
+        list.add("java");
+
+        System.out.println(list);
+    }
+}
